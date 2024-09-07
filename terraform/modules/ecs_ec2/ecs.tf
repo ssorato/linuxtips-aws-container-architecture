ce1f27c786ab596e00fc468ef5d2575b3b4de4f0
Simone Sorato <srtsmn@gmail.com>
2024-09-07 13:41:14 -0300

    added spot capacity provider

--- terraform/modules/ecs_ec2/ecs.tf
+++ terraform/modules/ecs_ec2/ecs.tf
@@ -17,7 +17,8 @@ resource "aws_ecs_cluster" "main" {
 resource "aws_ecs_cluster_capacity_providers" "main" {
   cluster_name = aws_ecs_cluster.main.name
   capacity_providers = [
-    aws_ecs_capacity_provider.on_demand.name
+    aws_ecs_capacity_provider.on_demand.name,
+    aws_ecs_capacity_provider.spot.name
   ]
 
   default_capacity_provider_strategy {
@@ -43,6 +44,27 @@ resource "aws_ecs_capacity_provider" "on_demand" {
   tags = merge(
     {
       Name = format("on-demand-ecs-%s", var.project_name)
+    },
+    var.common_tags
+  )
+}
+
+resource "aws_ecs_capacity_provider" "spot" {
+  name = format("spot-ecs-%s", var.project_name)
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.spot.arn
+    managed_scaling {
+      maximum_scaling_step_size = 10
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 90
+    }
+  }
+
+  tags = merge(
+    {
+      Name = format("spot-ecs-%s", var.project_name)
     },
     var.common_tags
   )
