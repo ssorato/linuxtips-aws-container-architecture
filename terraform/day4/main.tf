bd1bbc8abad80b5f19d0ddd9f8ca8c3fbc628bcc
Simone Sorato <srtsmn@gmail.com>
2024-09-15 17:53:20 -0300

    push image to ecr

--- terraform/day4/main.tf
+++ terraform/day4/main.tf
@@ -2,6 +2,7 @@ data "http" "my_public_ip" {
   url = "http://ifconfig.me"
 }
 
+
 module "ecs_app" {
   source = "git::https://github.com/ssorato/linuxtips-aws-container-architecture-tf-modules.git//ecs_app?ref=day4"
 
@@ -14,4 +15,18 @@ module "ecs_app" {
   ecs_service             = var.ecs_service
   common_scale            = var.common_scale
   cloudwatch_scale        = var.cloudwatch_scale
+}
+
+# Pull chip image from Docker Hub and push to the ECR
+resource "docker_image" "chip" {
+  name = "fidelissauro/chip:latest"
+}
+
+resource "docker_tag" "ecr_chip" {
+  source_image = docker_image.chip.name
+  target_image = "${module.ecs_app.ecr_repo_url}:latest"
+}
+
+resource "docker_registry_image" "ecr_chip" {
+  name = docker_tag.ecr_chip.target_image
 }
--- terraform/day4/providers.tf
+++ terraform/day4/providers.tf
@@ -17,10 +17,25 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.6.2"
     }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
   }
 
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+data "aws_ecr_authorization_token" "token" {}
+
+provider "docker" {
+  registry_auth {
+      address = data.aws_ecr_authorization_token.token.proxy_endpoint
+      username = data.aws_ecr_authorization_token.token.user_name
+      password  = data.aws_ecr_authorization_token.token.password
+    }
 }
