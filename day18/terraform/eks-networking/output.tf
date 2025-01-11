1a73b551fee391c5e7ee4cc6820fe340192f2996
Simone Sorato <srtsmn@gmail.com>
2025-01-11 19:18:37 -0300

    day18: added module outputs

---
+++ day18/terraform/eks-networking/output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value       = module.esk-networking.vpc_id
+  description = "SSM Parameter about vpc id"
+}
+
+output "public_subnets" {
+  value       = module.esk-networking.public_subnets
+  description = "SSM Parameters about public subnets id"
+}
+
+output "private_subnets" {
+  value       = module.esk-networking.private_subnets
+  description = "SSM Parameters about private subnets id"
+}
+
+output "database_subnets" {
+  value       = module.esk-networking.database_subnets
+  description = "SSM Parameters about database subnets id"
+}
